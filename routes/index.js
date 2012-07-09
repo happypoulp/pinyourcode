b1a0a6d11b7337dd6aaa22c2d06d36127e1644de
François Bonnefont <happypoulp@gmail.com>
2012-07-09 09:23:06 +0200

    wip

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ exports.index = function(req, res)
         {
             title: 'Express JS Demo',
             FB_APP_ID: '397068970352801',
-            domain: '127.0.0.1:3000'
+            domain: 'localhost:3000'
         }
     );
 };

f030ddaf81fde27310bb04ef49e3a36e26d2c7e6
François Bonnefont <happypoulp@gmail.com>
2012-09-14 11:02:54 +0200

    I & co

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var index = function (req, res) {
   res.render(
     'index',
     {
-      title : 'So human',
+      title : 'I & Co' ,
       FB_APP_ID : '397068970352801',
       FB_APP_DOMAIN : 'localhost:3000'
     }

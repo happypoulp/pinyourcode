624e6a5eeef363be3a4594823af4e1cfe72894bd
François Bonnefont <happypoulp@gmail.com>
2012-10-15 15:46:13 +0200

    reindent

--- routes/api/friends.js
+++ routes/api/friends.js
@@ -34,132 +34,192 @@ module.exports = function (app)
         }
     });
   
-    app.get('/friends', api.check, function (req, res) {
-      db.collection('friends').find(
+    app.get('/friends', api.check, function (req, res)
+    {
+        db.collection('friends').find(
         {
-          user_id : req.facebook.user_id
+            user_id : req.facebook.user_id
         })
-        .toArray(function (err, friends) {
-          if ( err ) {
-            res.json(err, 500);
-          } else {
-            res.json(friends);
-          }
+        .toArray(function (err, friends)
+        {
+            if ( err )
+            {
+                res.json(err, 500);
+            }
+            else
+            {
+                res.json(friends);
+            }
         });
     });
   
-    app.post('/friends', api.check, function (req, res) {
-      // Parse the request body
-      friend_builder.build(req.facebook.user_id, req.body, function (err, friend) {
-        if ( err ) {
-          // Parsing error must be donne first, don't need to hit the db
-          res.json(err, 400);
-        } else {
-          // Hitting the db to known if the friend already exists
-          db.collection('friends').get(req.facebook.user_id, friend.fb_id, function (err, dbFriend) {
-            if ( err ) {
-              res.json(err, 500);
-            } else {
-              if ( dbFriend ) {
-                res.json('A document with this fb_id already exists for this user', 400);
-              } else {
-                db.collection('friends').insert(friend, function (err, result) {
-                  if ( err ) {
-                    res.json(err, 500);
-                  } else {
-                    res.json(result[0], 201);
-                  }
-                });
-              }
+    app.post('/friends', api.check, function (req, res)
+    {
+        // Parse the request body
+        friend_builder.build(req.facebook.user_id, req.body, function (err, friend)
+        {
+            if ( err )
+            {
+            // Parsing error must be donne first, don't need to hit the db
+            res.json(err, 400);
             }
-          });
-        }
-      });
+            else
+            {
+                // Hitting the db to known if the friend already exists
+                db.collection('friends').get(req.facebook.user_id, friend.fb_id, function (err, dbFriend)
+                {
+                    if ( err )
+                    {
+                        res.json(err, 500);
+                    }
+                    else
+                    {
+                        if ( dbFriend )
+                        {
+                            res.json('A document with this fb_id already exists for this user', 400);
+                        }
+                        else
+                        {
+                            db.collection('friends').insert(friend, function (err, result)
+                            {
+                                if ( err )
+                                {
+                                    res.json(err, 500);
+                                }
+                                else
+                                {
+                                    res.json(result[0], 201);
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        });
     });
   
-    app.get('/friends/:fb_id', api.check, function (req, res) {
-      db.collection('friends').get(req.facebook.user_id, req.params.fb_id, function (err, friend) {
-        if ( err ) {
-          res.send(err, 500);
-        } else {
-          if ( friend ) {
-            res.json(friend);
-          } else {
-            res.json('No document found with this fb_id', 404);
-          }
-        }
-      });
-    });
-  
-    app.put('/friends/:fb_id', api.check, function (req, res) {
-      if (!req.body.fb_id) req.body.fb_id = req.params.fb_id;
-      // Parsing the request body
-      friend_builder.build(req.facebook.user_id, req.body, function (err, friend) {
-        if ( err ) {
-          // Parsing error must be donne first, don't need to hit the db
-          res.json(err, 400);
-        } else {
-          // Checking cohesion between document and url
-          if ( req.params.fb_id === friend.fb_id ) {
-            db.collection('friends').get(req.facebook.user_id, req.params.fb_id, function (err, dbFriend) {
-              if ( err ) {
+    app.get('/friends/:fb_id', api.check, function (req, res)
+    {
+        db.collection('friends').get(req.facebook.user_id, req.params.fb_id, function (err, friend)
+        {
+            if ( err )
+            {
                 res.send(err, 500);
-              } else {
-                if ( dbFriend ) {
-                  // Update only the extensions section
-                  db.collection('friends').update(
-                    {
-                      user_id : req.facebook.user_id,
-                      fb_id : dbFriend.fb_id
-                    },
-                    {
-                      $set : {extensions : friend.extensions}
-                    },
-                    function (err, result) {
-                      if ( err ) {
-                        res.json(err, 400);
-                      } else {
-                        // Update dbFriend extensions and send it (manual merging)
-                        dbFriend.extensions = friend.extensions;
-                        res.json(dbFriend);
-                      }
-                    });
-                } else {
-                  res.json('No document found with this fb_id', 404);
+            }
+            else
+            {
+                if ( friend )
+                {
+                    res.json(friend);
                 }
-              }
-            });
-          } else {
-            res.json("The document's fb_id doesn't match the requested url", 400);
-          }
-        }
-      });
+                else
+                {
+                    res.json('No document found with this fb_id', 404);
+                }
+            }
+        });
     });
   
-    app.delete('/friends/:fb_id', api.check, function (req, res) {
-      db.collection('friends').get(req.facebook.user_id, req.params.fb_id, function (err, dbFriend) {
-        if ( err ) {
-          res.send(err, 500);
-        } else {
-          if ( dbFriend ) {
-            // Delete the friend
-            db.collection('friends').remove(
-              {
-                user_id : req.facebook.user_id,
-                fb_id : dbFriend.fb_id
-              },
-              function (err, result) {
-                if ( err ) {
-                  res.json(err, 500);
-                } else {
-                  res.json("Deleted", 204);
+    app.put('/friends/:fb_id', api.check, function (req, res)
+    {
+        if (!req.body.fb_id) req.body.fb_id = req.params.fb_id;
+        // Parsing the request body
+        friend_builder.build(req.facebook.user_id, req.body, function (err, friend)
+        {
+            if ( err )
+            {
+                // Parsing error must be donne first, don't need to hit the db
+                res.json(err, 400);
+            }
+            else
+            {
+                // Checking cohesion between document and url
+                if ( req.params.fb_id === friend.fb_id )
+                {
+                    db.collection('friends').get(req.facebook.user_id, req.params.fb_id, function (err, dbFriend)
+                    {
+                        if ( err )
+                        {
+                            res.send(err, 500);
+                        }
+                        else
+                        {
+                            if ( dbFriend )
+                            {
+                                // Update only the extensions section
+                                db.collection('friends').update(
+                                {
+                                    user_id : req.facebook.user_id,
+                                    fb_id : dbFriend.fb_id
+                                },
+                                {
+                                    $set : {extensions : friend.extensions}
+                                },
+                                function (err, result)
+                                {
+                                    if ( err )
+                                    {
+                                        res.json(err, 400);
+                                    }
+                                    else
+                                    {
+                                        // Update dbFriend extensions and send it (manual merging)
+                                        dbFriend.extensions = friend.extensions;
+                                        res.json(dbFriend);
+                                    }
+                                });
+                            }
+                            else
+                            {
+                                res.json('No document found with this fb_id', 404);
+                            }
+                        }
+                    });
                 }
-              });
-          } else {
-            res.json('No document found with this fb_id', 404);
-          }
-        }
-      });
+                else
+                {
+                    res.json("The document's fb_id doesn't match the requested url", 400);
+                }
+            }
+        });
+    });
+  
+    app.delete('/friends/:fb_id', api.check, function (req, res)
+    {
+        db.collection('friends').get(req.facebook.user_id, req.params.fb_id, function (err, dbFriend)
+        {
+            if ( err )
+            {
+                res.send(err, 500);
+            }
+            else
+            {
+                if ( dbFriend )
+                {
+                    // Delete the friend
+                    db.collection('friends').remove(
+                    {
+                        user_id : req.facebook.user_id,
+                        fb_id : dbFriend.fb_id
+                    },
+                    function (err, result)
+                    {
+                        if ( err )
+                        {
+                            res.json(err, 500);
+                        }
+                        else
+                        {
+                            res.json("Deleted", 204);
+                        }
+                    });
+                }
+                else
+                {
+                    res.json('No document found with this fb_id', 404);
+                }
+            }
+        });
     });
 
 }

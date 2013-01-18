9e7a5ca43396444d3ce64fd372edc4d5d8b101d5
François Bonnefont <happypoulp@gmail.com>
2013-01-18 18:49:49 +0100

    broke everything

---
+++ public/js/modules/application.js
@@ -0,0 +1,13 @@
+define(['jquery', 'underscore', 'backbone', 'router'], function($, _, Backbone, Router)
+{
+    var initialize = function()
+    {
+        console.log('Router initialize');
+        // Pass in our Router module and call it's initialize function
+        Router.initialize();
+    };
+
+    return { 
+        initialize: initialize
+    };
+});

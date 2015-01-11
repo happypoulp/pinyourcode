ca789c00e8ff18028c4c464c8381212e14104f6e
François Bonnefont <happypoulp@gmail.com>
2015-01-11 21:52:19 +0100

    Fix design update merge issue + new buttons design

--- public/js/modules/templates/extension/create.js
+++ public/js/modules/templates/extension/create.js
@@ -2,5 +2,5 @@ var jd_rt="jade-runtime";define([jd_rt], function(jade) { return function(locals
 var buf = [];
 var jade_mixins = {};
 
-buf.push("<h4>Add an extension</h4><div class=\"input_container\"><input type=\"text\" name=\"extension_name\" placeholder=\"name\"/></div><div class=\"input_container\"><input type=\"text\" name=\"extension_content\" placeholder=\"content\"/></div><div class=\"input_container\"><input type=\"text\" name=\"extension_tags\" placeholder=\"tags (comma separated: ex: yo, fun and magic)\"/></div><div class=\"submit_container\"><input type=\"submit\"/></div>");;return buf.join("");
+buf.push("<h4>Add an extension</h4><div class=\"input_container\"><input type=\"text\" name=\"extension_name\" placeholder=\"name\"/></div><div class=\"input_container\"><input type=\"text\" name=\"extension_content\" placeholder=\"content\"/></div><div class=\"input_container\"><input type=\"text\" name=\"extension_tags\" placeholder=\"tags (comma separated: ex: yo, fun and magic)\"/></div><div class=\"submit_container\"><input type=\"submit\" class=\"button green\"/></div>");;return buf.join("");
 } });

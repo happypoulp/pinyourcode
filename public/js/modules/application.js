2587ceeee419fc72333237ec7eb939013f5ebe55
François Bonnefont <happypoulp@gmail.com>
2015-01-16 00:25:35 +0100

    wip start webpack + reflux

--- public/js/modules/application.js
+++ public/js/modules/application.js
@@ -1,40 +1,14 @@
-(function()
+var Router = require('router');
+var Facebook = require('facebook');
+
+var Application = function()
 {
-    var moduleName = 'application';
-
-    define([
-            'router',
-            'facebook',
-            'pubsub', // Just for packing
-            'jade-runtime' // Just for packing
-        ], function(Router, Facebook)
+    Facebook.init(function()
     {
-        log(moduleName, "Dependencies loaded", "Build module");
-
-        var Application = function() {};
-
-        Application.prototype =
-        {
-            initialize: function()
-            {
-                log(moduleName, 'initialize');
-
-                var that = this;
-
-                $(function()
-                {
-                    log(moduleName, 'Facebook.init');
-                    // that.initHandlers();
-                    Facebook.init(function()
-                    {
-                        log(moduleName, 'Facebook.init - DONE');
-                        log(moduleName, 'Router.initialize');
-                        Router.initialize();
-                    });
-                });
-            }
-        };
-
-        return new Application();
+        log('Facebook.init - DONE');
+        log('Router.initialize');
+        Router.initialize();
     });
-})();
+};
+
+Application();
